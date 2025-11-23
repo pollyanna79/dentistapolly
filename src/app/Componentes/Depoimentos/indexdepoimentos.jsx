a40cc65ceca00e5aae90c5321ee0a6822f43c9d4
pollyanna <santossouzapollyanna79@gmail.com>
2025-11-23 15:00:10 -0300

    segundo commit

---
+++ src/app/Componentes/Depoimentos/indexdepoimentos.jsx
@@ -0,0 +1,17 @@
+import estilos from './depoimentos.module.css';
+import Image from 'next/image';
+
+export default function CardDepoimento(props){
+    return(
+        < div className={estilos.card_depoimento}>
+        <Image className={estilos.img_aparelho}
+        src={props.imagem}
+        alt={props.nome}
+        title={props.nome}
+        />
+        <h3>{props.nome}</h3>
+        <p>{props.descricao}</p>
+        </div>
+
+    )
+}
